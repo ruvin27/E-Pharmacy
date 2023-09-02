13fabd02dccd3211c439e570eaf1c4834e3d5cc0
Ruvin Rodrigues <ruvin27@gmail.com>
2023-09-02 18:40:37 -0500

    added folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,27 @@
 const express = require('express')
-
+const firebaseConfig = require('../src/firebase')
 const app = express()
 
 app.listen(4000, () =>{
     console.log('listening!')
 })
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://e-pharmacy-e4d2e-default-rtdb.firebaseio.com"
+});
+
+// const db = admin.database();
+// const ref = db.ref('users'); // Replace with the path you want to query
+
+// ref.once('value', (snapshot) => {
+//   const data = snapshot.val();
+//   console.log(data);
+// })
+// .catch((error) => {
+//   console.error('Error fetching data:', error);
+// });
---
+++ backend/serviceAccountKey.js
@@ -0,0 +1,15 @@
+const serviceAccountKey = {
+    "type": "service_account",
+    "project_id": "e-pharmacy-e4d2e",
+    "private_key_id": "c63ae9ff00d2c2ac956a9c8215242d655187f268",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDrHMz3kR5Nb515\nefeV/P2X5//QCHwBP4yUV6kA4GR1xF6iuIYE4S5G5xLh/Kxtt2WBXp2+GKLvcJyW\nT7QDJX0IR8zmIIsbiTvokUtf02silQEGekxWtn11nBuX9i676cl6rW5UvzT4RNqU\n6rd0Uyd9CWpFpSKAtXvoj2JuvYuCc3qxAU013iJgdEQ3AZMSv8RF4eiNalibClZE\nfDWEctk7r3tBd2myYJigwlDjjs6FeRVyDC5wreiMHZLgah0QeFddzYWyOiuDgKdq\nXRRa1oyMmwSjGjVOQMinoyFHsD9pbNdcBOSBENZwGwxtz/3LBM7vkW1X4UhcsNmF\nXX57nntBAgMBAAECggEAURbsXKCJFoUeB5U4/tq0bk2ejdY3SsjryAgZ6or6KyIQ\nz8ebtZgiuWKeQSJAKFE+KaoD9toRbErnPvJ7cDBgWNhGw8JKqDMjXfmqyQCmrVU9\n9sl8hdXIHKW8YKOQh9BA5NZnPMR71PsOdpkjr/O0JWx3YlDrvv+ZHmNBvFcUhGd9\nlOzhn9NW26s5Eze/UQdFaa297yajYGpYvbnyRdbzU9B5wxwZ1j8Iwjaf0kBvRyu4\nmc8FwElgGrWCtlfiA9vjmfCy/galK8MjS0MhLCRi2ZOIh7z8REsrhUku2ZO0CHSH\nSe8lwx58DYQNxcDzBWuUJt1KjXtEDwa/FaqwYkCQzwKBgQD4haUbiChjYOToGL5P\nCHvDIw+klTWoIp/gmFdV6sQHKCsx/hJ2OyYtiNxVDaawtNZZB+ocuHZFSqjZSuSr\n/vhyg+KK1WeXBuqmfq/GVkS13bup4gdmn06+2MWylDfaD7/6im1aB/UoOKI1rp10\nQgdObEwmjZmoYQ/le7+YeK2/dwKBgQDyL9zNYEEIarpzh3DROQ/2rtg6KaiYlXsO\nQEx9nOtk5MiXKn2f6BcNyLAZ9pnd9DWsGvK9D/NFgDF5VFi32eLLv/NLMuETHU4f\n4WfLTKgvK8pWqCmWwnoxK8RoI/qfe7PaeYekoeOcXg6ybXaP0/iElE+Mw0VwSYSD\nYz7AOVl5BwKBgFAsHQVyuUTF/1J4mPw6CmL7LpKzp+mjsdvsiVAoOjDs3nJ0OlR1\n5G0QF7RAgSPEUtnJ2daGE3FrEml25aouyMt4jF6pVY+9JXmsqNvc/LGG5IF7b1PF\n1MtwduaTNilcp1wVyA0hQA9HpdyXhif5HQ5JlY9iYs2U8mHfuAFh/PslAoGBAMNT\nRsl87/ZR3b5/ZcXYN7/ElC+UmZ02MeD0irxkapUcz4rbgiYNkUpUqXLHKKGR7Be6\nRDunOZS8JyqrlEHzQK8DSL1Fc7Fu2SttJ8EXOi2RK8doKOKmRtaRbf5vxhoKMqvp\niDSHPINIeKH/Frg5RR3oX6AvfTlqka1Fxb5Sgo7RAoGABPVKGOVVPDlsRZg59iL8\n4hKhCnBBA1q25Nf4qfIzwThRhRM0DvooE+IdO2SQnmv+or75KNq1w2pnWtRHJbD+\nDMo/bIq9PCH0ITkh2rbEdqQcn3VR31GawoBVwNX4kfvd9pcWj0ozkBQafVojLIYE\nZnSY0Gu+AbN0oC1oh734vjY=\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-90vug@e-pharmacy-e4d2e.iam.gserviceaccount.com",
+    "client_id": "107584017269698647494",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-90vug%40e-pharmacy-e4d2e.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  }
+
+module.exports = serviceAccountKey;
